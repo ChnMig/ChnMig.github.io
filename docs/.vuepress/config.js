668cb67843ebb98c0bc0aa2e7a7fca8a14f007a6
ChnMig <chnmig@outlook.com>
2021-07-27 13:32:44 +0800

    update config.js and github workflows

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -9,6 +9,21 @@ module.exports = {
   themeConfig: {
     logo: '/images/avatar.gif',
     repo: 'https://github.com/ChnMig',
+    editLinkText: '在 GitHub 上编辑此页',
+    lastUpdatedText: '上次更新',
+    contributorsText: '贡献者',
+    tip: '提示',
+    warning: '注意',
+    danger: '警告',
+    backToHome: '返回首页',
+    openInNewWindow: '在新窗口打开',
+    toggleDarkMode: '切换夜间模式',
+    notFound: [
+      '这里什么都没有',
+      '我们怎么到这来了？',
+      '这是一个 404 页面',
+      '看起来我们进入了错误的链接',
+    ],
     navbar: [
       {
         text: '访问数据',
