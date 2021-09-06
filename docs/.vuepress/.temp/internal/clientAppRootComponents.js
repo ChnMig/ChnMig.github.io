792f2337b2da3fcb14464989d3440bcb5c20a2b3
ChnMig <chnmig@outlook.com>
2021-09-06 10:06:50 +0800

    update

--- docs/.vuepress/.temp/internal/clientAppRootComponents.js
+++
@@ -1,5 +0,0 @@
-import clientAppRootComponent0 from '/Users/chenming/Work/ChnMig.github.io/node_modules/@vuepress/plugin-back-to-top/lib/client/components/BackToTop.js'
-
-export const clientAppRootComponents = [
-  clientAppRootComponent0,
-]
--- docs/.vuepress/.temp/pages/about.html.vue
+++
@@ -1,3 +0,0 @@
-<template><h1 id="关于我" tabindex="-1"><a class="header-anchor" href="#关于我" aria-hidden="true">#</a> 关于我</h1>
-<p>我是一名很菜的后端开发者, 所以为了不被甩开, 必须要加油进步才行</p>
-</template>
